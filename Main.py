a4480c10a1d9d43d430ac78b424e729a54bd76e2
T_cat9000_2 <terpstragraham@gmail.com>
2024-10-20 09:30:15 -0700

    bring back older&longer supported method

--- Main.py
+++ Main.py
@@ -1,18 +1,24 @@
 import platform
 import os
 import subprocess
-#fixes directory issues
-base_dir = os.path.dirname(__file__)
+
+def get_username():
+  if platform.system() == "Windows":
+    return os.getlogin()  # or os.environ['USERNAME']
+  else:
+    return os.getenv('USER')
+
+username = get_username()
 
 if platform.system() == "Windows":
-  cmd_location = os.path.join(base_dir, "commandline.py")
+  cmd_location = rf'C:\Users\{username}\downloads\Sola-1.1\commandline.py'
 else:
-  cmd_location = os.path.join(base_dir, "commandline.py")
+  cmd_location = f'/home/{username}/downloads/Sola-1.1/commandline.py'
 
 if platform.system() == "Windows":
-  ui_location = os.path.join(base_dir, "ui.py")
+  ui_location = rf'C:\Users\{username}\downloads\Sola-1.1\ui.py'
 else:
-  ui_location = os.path.join(base_dir, "ui.py")
+  ui_location = f'/home/{username}/downloads/Sola-1.1/ui.py'
 
 option = input("Welcome to Sola! would you like to use the simple command line player or the UI player? [cmd/ui]: ")
 if option == "cmd":
